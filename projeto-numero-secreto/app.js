c604b47d1f25ac26907666dcccb67cc159f3c672
Kaillanny Fontana dos Santos <163677901+kai-fontana@users.noreply.github.com>
2025-07-17 18:54:41 -0300

    Add files via upload

--- projeto-numero-secreto/app.js
+++ projeto-numero-secreto/app.js
@@ -4,20 +4,34 @@ titulo.innerHTML = 'Jogo do número secreto';*/
 let paragrafo = document.querySelector("p");
 paragrafo.innerHTML = "Escolha um número entre 1 e 10";
 
-let numeroSecreto = gerarNumeroAleatorio()
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto){
-   let campo = document.querySelector(tag);
-   campo.innerHTML = texto;
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
 }
 
 exibirTextoNaTela("h1", "Jogo do número secreto");
 exibirTextoNaTela("p", "Escolha um número entre 1 e 100.");
 
 function verificarChute() {
-   let 
+  let chute = document.querySelector("input").value;
+
+  if (chute == numeroSecreto) {
+    exibirTextoNaTela("h1", "Acertou!");
+    let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+    let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+    exibirTextoNaTela("p", mensagemTentativas);
+  } else {
+    if (chute > numeroSecreto) {
+      exibirTextoNaTela("p", "O número secreto é menor!");
+    } else {
+      exibirTextoNaTela("p", "O número secreto é maior!");
+    }
+  }
 }
 
 function gerarNumeroAleatorio() {
-   return parseInt(Math.random() * 10 + 1);
+  return parseInt(Math.random() * 10 + 1);
 }
